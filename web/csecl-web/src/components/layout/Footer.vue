b695eccc9ce915806b74fcc08efd71787bd18010
JooZh <476506353@qq.com>
2017-10-11 22:40:52 +0800

    页面基本完成等待bug测试

---
+++ web/csecl-web/src/components/layout/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .footer
+    position absolute
+    left 0
+    bottom  0
+    right 0
+    height 50px
+    background #999
+</style>
+
